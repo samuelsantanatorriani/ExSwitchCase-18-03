2e2dd12b8a34f41e3c5d4c5ebafeec2d8547a416
Guilhermesilvaandre <guilhermeaandre2018@gmail.com>
2025-03-20 21:18:15 -0300

    Finalizando o exercicio 2

--- exercicios/ex2/calcular.php
+++ exercicios/ex2/calcular.php
@@ -1,50 +1,48 @@
 <?php
     $nome = $_POST['nome'];
-    $lanche = $_POST['lanche'];
+    $livro = $_POST['livros'];
     $ql = $_POST['ql'];
-    $bebida = $_POST['bebida'];
     $total = 0;
 
-    switch ($lanche) {
+    switch ($livros) {
         case "1":
-            $total = $total + (6 * $ql);
+            $total = $total + (20 * $ql);
             break;
         case "2":
-            $total = $total + (8 * $ql);
+            $total = $total + (28 * $ql);
             break;
         case "3":
-            $total = $total + (10 * $ql);
+            $total = $total + (30 * $ql);
             break;
         case "4":
-            $total = $total + (12 * $ql);
+            $total = $total + (29 * $ql);
             break;
         case "5":
-            $total = $total + (15 * $ql);
-            break;    
+            $total = $total + (35 * $ql);
+            break;
+
+        case "6":
+            $total = $total + (32 * $ql);
+            break;
+        case "7":
+            $total  = $total + (40 * $ql);
+            break;
+        case "8":
+            $total = $total + (43 * $ql);
+            break;
+        case "9":
+            $total = $total + (45* $ql);
+            break;
+        case "10":
+            $total = $total + (48 * $ql);
+            break;
+             
+
+
         default:
             echo "Opção inválida.";
     }
 
-    switch ($bebida) {
-        case "1":
-            $total = $total + 2;
-            break;
-        case "2":
-            $total = $total + 5;
-            break;
-        case "3":
-            $total = $total + 3;
-            break;
-        case "4":
-            $total = $total + 5;
-            break;
-        case "5":
-            $total = $total + 8;
-            break;    
-        default:
-            echo "Opção inválida.";
-    }
 
-
-    echo "O pedido do(a) ".$nome." ficou em: R$".$total.",00"
+    echo "A compra do(a) ".$nome." ficou em: R$".$total.",00"
 ?>
--- exercicios/ex2/index.php
+++ exercicios/ex2/index.php
@@ -10,38 +10,37 @@
     Digite o seu nome:
     <input type="text" name="nome" size="25" /><br><br>
 
-        Menu de livros: <br><br>
+        livros a venda: <br><br>
         1 – Dom Quixote     (R$20,00) <br>
-        2 – Moby Dick      (R$28,00) <br>
-        3 – Orgulho e Preconceito      (R$30,00) <br>
-        4 – Crime e Castigo      (R$29,00) <br> 
-        5 – Os Irmãos Karamázov   (R$35,00) <br>
-        6 -                      (32,00)<br>
-        <label for="lanche">Escolha lanche:</label>
+        2 – Moby Dick   (R$28,00) <br>
+        3 – Orgulho e Preconceito   (R$30,00) <br>
+        4 – Crime e Castigo     (R$29,00) <br> 
+        5 – Os Irmãos Karamázov     (R$35,00) <br>
+        6 - O Grande Gatsby     (32,00)<br>
+        7 - A Odisséia       (40,00)<br>
+        8 - Cem Anos de Solidão     (43,00)<br>
+        9 - Frankenstein        (45,00)<br>
+        10 - 48 leis do poder    (48,00)<br><br>
+
+
+        <label for="livros">Escolha os livros:</label>
         <select name="livros" id="livros">
             <option value="1"> Dom Quixote   </option>
             <option value="2">Moby Dick  </option>
             <option value="3">Orgulho e Preconceito</option>
             <option value="4">Crime e Castigo </option>
             <option value="5">Os Irmãos Karamázov </option>
+            <option value="6"> O Grande Gatsby </option>
+            <option value="7"> A Odisséia </option>
+            <option value="8"> Cem Anos de Solidão </option>
+            <option value="9"> Frankenstein  </option>
+            <option value="10"> 48 leis do poder </option>
+            
         </select><br><br>
-    Digite a quantidade de lanches:
+    Digite quantos livros:
     <input type="text" name="ql" size="25" /><br><br>
-<!--         
-        Menu Bebidas: <br><br>
-        1 – Água (R$2,00) <br>
-        2 – Suco (R$5,00) <br>
-        3 – Lata (R$3,00) <br>
-        4 – 600 ml (R$5,00) <br>
-        5 – 2 Litros (R$8,00) <br><br>
-        <label for="bebida">Escolha bebida:</label>
-        <select name="bebida" id="bebida">
-            <option value="1">Água  </option>
-            <option value="2">Suco</option>
-            <option value="3">Lata </option>
-            <option value="4">600 ml</option>
-            <option value="5">2 Litros</option>
-             -->
+        
+      
         </select>
         <button type="submit">Enviar</button>
     </form>
@@ -53,8 +52,3 @@
 
 
 
-<!-- O Grande Gatsby – F. Scot
-1984 –
-A Odisséia – Ho
-Cem Anos de Solidão – Gabriel Garcí
-Frankenstein– Maria Shelle -->
